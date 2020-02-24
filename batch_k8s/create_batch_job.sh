f191fc8bbcccc5d4dec7d05dbdcee68b75168baa
Jiaxin Li <jiaxin.1995@icloud.com>
2020-02-23 16:21:25 -0800

    checkpoint; fix batch scripts

--- batch_k8s/create_batch_job.sh
+++ batch_k8s/create_batch_job.sh
@@ -2,4 +2,8 @@ if [ "$#" -lt 1 ]; then
   echo "Usage: $0 <experiment code> " >&2
   exit 1
 fi
-cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | kubectl create -f -
+if [ "$#" == 2 ]; then
+  cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | sed "s/DATA_F/$2/g" | kubectl create -f -
+else 
+  cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | sed "s/DATA_F//g" | kubectl create -f -
+fi
--- batch_k8s/delete_batch_job.sh
+++ batch_k8s/delete_batch_job.sh
@@ -2,4 +2,8 @@ if [ "$#" -lt 1 ]; then
   echo "Usage: $0 <experiment code> " >&2
   exit 1
 fi
-cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | kubectl delete -f -
+if [ "$#" == 2 ]; then
+  cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | sed "s/DATA_F/$2/g" | kubectl delete -f -
+else 
+  cat batch_job.yaml | sed "s/EXP_CODE/$1/g" | sed "s/DATA_F//g" | kubectl delete -f -
+fi
